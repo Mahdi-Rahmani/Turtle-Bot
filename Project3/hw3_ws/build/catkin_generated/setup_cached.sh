16d1f74b53baf345e7bf6f74a96695d58e2524c5
Mahdi-Rahmani <mah.rah79@yahoo.com>
2023-06-02 01:35:33 +0330

    step3 code is added. This part is not complete and PID gains need to be changed

--- Project3/hw3_ws/build/catkin_generated/setup_cached.sh
+++ Project3/hw3_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/mahdi/Desktop/Robotics/project3/hw3_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/mahdi/Desktop/Robotics/project3/hw3_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/mahdi/Desktop/Robotics/project3/hw3_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/mahdi/Desktop/Robotics/project3/hw3_ws/build'
+export PYTHONPATH="/home/mahdi/Desktop/Robotics/project3/hw3_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mahdi/Desktop/Robotics/project3/hw3_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/mahdi/Desktop/Robotics/project3/hw3_ws/src:$ROS_PACKAGE_PATH"
